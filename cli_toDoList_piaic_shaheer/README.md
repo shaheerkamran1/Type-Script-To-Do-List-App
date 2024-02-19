037d2559eeed8ba5cc18ae4d84b0dce395d3d3dc
Muhammad Shaheer Kamran <112251353+shaheerkamran1@users.noreply.github.com>
2024-02-19 22:42:06 +0500

    Add files via upload

---
+++ cli_toDoList_piaic_shaheer/README.md
@@ -0,0 +1,54 @@
+# TypeScript To-Do List Application using chalk and inquirer - Muhammad Shaheer Kamran
+
+A cli To Do List Application  designed witht the help of chalk and developed using TypeScript. 
+
+This is a console based application which allows the user to input his/her name and the system starts. Which then asked the user to select from one of the given options, options comprising of 
+-Enter task
+-View all tasks present
+-Delete Task (Last task or complete list)
+
+This program is made for the sole purpose of practice and showing skills based upon TypeScript language.
+
+
+# Download the Package
+
+```
+npm i cli_toDoList_piaic_shaheer
+
+```
+
+# Run the To-Do List App on your  VSCode 
+
+```
+npx cli_toDoList_piaic_shaheer
+
+```
+
+# Packages
+
+[chalk](https://github.com/chalk/chalk) | 
+[inquirer](https://github.com/SBoudrias/Inquirer.js)
+
+# Inquirer
+
+```
+npm i inquirer
+```
+```
+npm --save-dev @types/inquirer -D
+```
+
+# Chalk
+```
+npm i chalk
+```
+```
+npm i @types/inquirer -D
+npm i @types/chalk -D
+```
+
+# Node Modules
+```
+npm i node
+npm i @types/node
+```
---
+++ cli_toDoList_piaic_shaheer/app.js
@@ -0,0 +1,106 @@
+//Develop a simple command line Todo app using TypeScipt, Node.js and Inquirer.
+//Create a GitHub repository for the 
+//project and submit its URL in the project submission form.
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const green = chalk.greenBright.bold;
+const red = chalk.redBright.bold;
+const yellow = chalk.yellowBright.bold.underline;
+const blue = chalk.blueBright;
+const whites = chalk.whiteBright.bold;
+console.log(red("████████╗ █████╗     ██████╗  █████╗     ██╗     ██╗ ██████╗████████╗     █████╗ ██████╗ ██████╗ "));
+console.log(red("╚══██╔══╝██╔══██╗    ██╔══██╗██╔══██╗    ██║     ██║██╔════╝╚══██╔══╝    ██╔══██╗██╔══██╗██╔══██╗"));
+console.log(red("   ██║   ██║  ██║    ██║  ██║██║  ██║    ██║     ██║╚█████╗    ██║       ███████║██████╔╝██████╔╝"));
+console.log(red("   ██║   ██║  ██║    ██║  ██║██║  ██║    ██║     ██║ ╚═══██╗   ██║       ██╔══██║██╔═══╝ ██╔═══╝ "));
+console.log(red("   ██║   ╚█████╔╝    ██████╔╝╚█████╔╝    ███████╗██║██████╔╝   ██║       ██║  ██║██║     ██║     "));
+console.log(red("   ╚═╝    ╚════╝     ╚═════╝  ╚════╝     ╚══════╝╚═╝╚═════╝    ╚═╝       ╚═╝  ╚═╝╚═╝     ╚═╝     "));
+console.log("\n\n");
+console.log(blue(`Ｄｅｖｅｌｏｐｅｄ  ｗｉｔｈ ${whites('❤')}  ｂｙ  Ｓｈａｈｅｅｒ`));
+console.log("\n\n");
+let myTask = [];
+let userInput = await inquirer.prompt([{
+        name: "name",
+        type: "string",
+        message: blue("Enter your name: ")
+    }]);
+console.log(green(`\nWelcome ${userInput.name} to To-Do List App\n`));
+async function promptUser() {
+    let prompting = await inquirer.prompt([{
+            name: "command",
+            type: "list",
+            choices: ["1. Enter Task", "2. View Tasks", "3. Delete Task", "4. Exit"],
+            message: blue("Select your command: :")
+        }]);
+    switch (prompting.command) {
+        case "1. Enter Task": {
+            let userTask = await inquirer.prompt([
+                {
+                    name: "task",
+                    type: "string",
+                    message: blue("Enter your task: "),
+                }
+            ]);
+            myTask.push(userTask.task);
+            console.log(green("\nTask Added successfully!\n"));
+            promptUser();
+            break;
+        }
+        case "2. View Tasks": {
+            if (myTask.length != 0) {
+                for (let i = 0; i < myTask.length; i++) {
+                    console.log(yellow(`Task ${i}: ${myTask[i]}\n`));
+                }
+            }
+            else if (myTask.length == 0) {
+                console.log(red("\nYour Task list is empty\n"));
+            }
+            promptUser();
+            break;
+        }
+        case "3. Delete Task": {
+            let del = await inquirer.prompt([{
+                    name: "task",
+                    type: "list",
+                    choices: ["1. Delete latest task", "2. Delete all tasks"]
+                }]);
+            if (del.task == "1. Delete latest task") {
+                if (myTask.length >= 1) {
+                    myTask.pop();
+                    console.log(green("\nLatest Task deleted successfully\n"));
+                }
+                else if (myTask.length == 0) {
+                    console.log(red("\nYour list is empty already\n"));
+                }
+            }
+            else if (del.task == "2. Delete all tasks") {
+                let taskNo = await inquirer.prompt([{
+                        name: "index",
+                        type: "confirm",
+                        message: yellow("Confirm to delete all tasks?: "),
+                    }]);
+                if (taskNo.index == true) {
+                    if (myTask.length != 0) {
+                        while (myTask.length != 0) {
+                            myTask.pop();
+                        }
+                        console.log(green("\nYour Task list has been formatted successfully.\n"));
+                    }
+                    else if (myTask.length == 0) {
+                        console.log(red("\nYour List is empty already.\n"));
+                    }
+                }
+            }
+            promptUser();
+            break;
+        }
+        case "4. Exit": {
+            console.log(green("\nThank you for using To-Do-List\n"));
+            break;
+        }
+        default: {
+            console.log(red("\nNothing\n"));
+            break;
+        }
+    }
+}
+promptUser();
